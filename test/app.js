c9fdbda825b7d740aef46d9fd9339831984b0d35
Johnny Hall <johnny@recipher.co.uk>
2016-01-04 15:38:00 +0000

    Initial commit

---
+++ test/app.js
@@ -0,0 +1,14 @@
+var should = require('chai').should()
+  , sut = require('../lib/app');
+
+describe('app', function() {
+  
+  var suite = this;
+
+  before(function() {
+  });
+  
+  it('should', function() {
+  });
+  
+});
---
+++ test/worker.js
@@ -0,0 +1,14 @@
+var should = require('chai').should()
+  , sut = require('../lib/worker');
+
+describe('worker', function() {
+  
+  var suite = this;
+
+  before(function() {
+  });
+  
+  it('should', function() {
+  });
+  
+});
